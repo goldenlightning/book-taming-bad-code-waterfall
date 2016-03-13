e70f2a969f807aad5d7f30afccebb7c0db9e764e
goldenlightning <goldenlightning@163.com>
2016-03-14 00:18:28 +0800

    Added static method Clock.toLocalTime().

--- src/tbc/waterfall/Clock.java
+++ src/tbc/waterfall/Clock.java
@@ -2,9 +2,13 @@ package tbc.waterfall;
 
 public abstract class Clock {
 
-	private final int UTC_OFFSET = 0;
+	private static final int UTC_OFFSET = 0;
 	private int localTime = 0;
 	
 	public abstract void setLocalTime (int localTime);
+
+	public static int toLocalTime(int utcZeroTime) {
+		return utcZeroTime + UTC_OFFSET;
+	}
 	
 }
