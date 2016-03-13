00a50945c013d4aa11a17216c6852f095514795e
goldenlightning <goldenlightning@163.com>
2016-03-14 00:20:52 +0800

    Generated getter and setter of the field utcZeroTime of class UtcTime.

--- src/tbc/waterfall/UtcTime.java
+++ src/tbc/waterfall/UtcTime.java
@@ -2,10 +2,21 @@ package tbc.waterfall;
 
 public class UtcTime extends TimeSubject {
 
+	private int utcZeroTime;
+
+	public int getUtcZeroTime() {
+		return utcZeroTime;
+	}
+
+	public void setUtcZeroTime(int utcZeroTime) {
+		this.utcZeroTime = utcZeroTime;
+	}
+
 	@Override
 	public void notifyAllClocks() {
-		// TODO Auto-generated method stub
-
+		for(Clock clock : super.clocks.values()) {
+			clock.setLocalTime(Clock.toLocalTime(this.utcZeroTime));
+		}
 	}
 
 }
