6bd0dc3631b555c4e8735ab2e9521660739cb386
goldenlightning <goldenlightning@163.com>
2016-03-14 23:10:55 +0800

    Updated the main() method to make the constructor of PhoneClock is the same with CityClock and add the UtcTime object to the phoneClock using method PhoneClock.setUtcTime().

--- src/tbc/waterfall/HotelWorldClocksRunner.java
+++ src/tbc/waterfall/HotelWorldClocksRunner.java
@@ -3,6 +3,17 @@ package tbc.waterfall;
 public class HotelWorldClocksRunner {
 
 	public static void main(String[] args) {
+		TimeSubject utcTime = new UtcTime();
+		utcTime.attach("beijing", new CityClock(8));
+		utcTime.attach("london", new CityClock(0));
+		utcTime.attach("moscow", new CityClock(4));
+		utcTime.attach("sydney", new CityClock(10));
+		utcTime.attach("newYork", new CityClock(-5));
+		
+		Clock phoneClock = new PhoneClock(8);
+		phoneClock.setUtcTime(utcTime);
+		
+		utcTime.printTimeOfAllClocks();
 		
 	}
 	
