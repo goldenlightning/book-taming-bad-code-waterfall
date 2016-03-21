e81cf241f15bae69cc306a84d0bbea1aa736345c
goldenlightning <goldenlightning@163.com>
2016-03-21 23:22:56 +0800

    The filed UTC_OFFSET of class Clock must not be static so that all subclasses could have UTC_OFFSET.That's the root cause of the bug that the local time of all city clocks are set to the same time.

--- src/tbc/waterfall/Clock.java
+++ src/tbc/waterfall/Clock.java
@@ -2,13 +2,21 @@ package tbc.waterfall;
 
 public abstract class Clock {
 
-	protected static final int UTC_OFFSET = 0;
+	protected int UTC_OFFSET = 0;
 	protected int localTime = 0;
 	
+	public Clock(int utcOffset) {
+		UTC_OFFSET = utcOffset;
+	}
+
 	public abstract void setLocalTime (int localTime);
 
 	public static int toLocalTime(int utcZeroTime) {
 		return utcZeroTime + UTC_OFFSET;
+	}
+
+	public String getTime() {
+		return String.valueOf(this.localTime);
 	}
 	
 }
